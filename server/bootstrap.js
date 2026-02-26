d67a46bfb8e40a7c07b744b76f24466d1148dbea
chris <earthaccount+gh@mailbox.org>
2026-02-26 13:56:06 +0100

    feat(bootstrap): save-guard api calls

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,21 +2,26 @@
 const { getFullPopulateObject } = require("./helpers");
 
 module.exports = ({ strapi }) => {
+  const defaultDepth =
+    strapi
+      .plugin("strapi-v5-plugin-populate-deep")
+      ?.config("defaultDepth") || 5;
+
   // Subscribe to the lifecycles that we are intrested in.
   strapi.db.lifecycles.subscribe((event) => {
-    if (event.action === "beforeFindMany" || event.action === "beforeFindOne") {
-      const level = event.params?.pLevel;
+      const { action, model, params } = event;
+      // Skip if not matching db interaction
+      if (!["beforeFindMany", "beforeFindOne"].includes(action)) return;
+      // Skip if db query isn't targeting api::
+      if (!model.uid.startsWith("api::")) return;
+      // Skip if request url isn't api call
+      const ctx = strapi.requestContext.get();
+      if (!ctx?.request?.url?.startsWith("/api/")) return;
 
-      const defaultDepth =
-        strapi
-          .plugin("strapi-v5-plugin-populate-deep")
-          ?.config("defaultDepth") || 5;
+      const pLevel = params?.pLevel ?? ctx.query?.pLevel;
+      if (pLevel === undefined) return;
 
-      if (level !== undefined) {
-        const depth = level ?? defaultDepth;
-        const modelObject = getFullPopulateObject(event.model.uid, depth, []);
-        event.params.populate = modelObject.populate;
-      }
-    }
+      const depth = pLevel ? parseInt(pLevel, 10) : defaultDepth;
+      params.populate = getFullPopulateObject(model.uid, depth, []).populate;
   });
 };
